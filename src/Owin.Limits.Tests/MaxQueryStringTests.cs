990044bbbdc1b15ed1db43903d3251949dc362cd
Damian Hickey <dhickey@gmail.com>
2014-03-16 21:57:42 +0100

    Code cleanup to be consistent with rest of code base

--- src/Owin.Limits.Tests/MaxQueryStringTests.cs
+++ src/Owin.Limits.Tests/MaxQueryStringTests.cs
@@ -1,49 +1,58 @@
-﻿using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Microsoft.Owin.Testing;
-using Xunit;
+﻿namespace Owin.Limits
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Microsoft.Owin.Testing;
+    using Xunit;
 
-namespace Owin.Limits {
-    public class MaxQueryStringTests {
+    public class MaxQueryStringTests
+    {
         [Fact]
-        public async Task When_max_queryString_length_is_10_then_a_request_with_9_should_be_served() {
-            var client = CreateClient(10);
+        public async Task When_max_queryString_length_is_10_then_a_request_with_9_should_be_served()
+        {
+            HttpClient client = CreateClient(10);
 
-            var request = await client.GetAsync("http://example.com?q=1234567");
+            HttpResponseMessage request = await client.GetAsync("http://example.com?q=1234567");
 
             request.StatusCode.Should().Be(HttpStatusCode.OK);
         }
 
         [Fact]
-        public async Task When_max_queryString_length_is_10_then_a_request_with_11_should_be_rejected() {
-            var client = CreateClient(10);
+        public async Task When_max_queryString_length_is_10_then_a_request_with_11_should_be_rejected()
+        {
+            HttpClient client = CreateClient(10);
 
-            var request = await client.GetAsync("http://example.com?q=123456789");
+            HttpResponseMessage request = await client.GetAsync("http://example.com?q=123456789");
 
             request.StatusCode.Should().Be(HttpStatusCode.RequestUriTooLong);
             request.ReasonPhrase.Should().Be("The (unescaped) querystring is too long. Only 10 characters are allowed.");
         }
-        [Fact]
-        public async Task When_max_queryString_length_is_10_then_a_request_with_escaped_length_greater_than_10_but_unescaped_lower_or_equal_than_10_should_be_serverd() {
-            var client = CreateClient(10);
 
-            var request = await client.GetAsync("http://example.com?q=%48%49%50%51%52%53%54");
+        [Fact]
+        public async Task
+            When_max_queryString_length_is_10_then_a_request_with_escaped_length_greater_than_10_but_unescaped_lower_or_equal_than_10_should_be_serverd
+            ()
+        {
+            HttpClient client = CreateClient(10);
+
+            HttpResponseMessage request = await client.GetAsync("http://example.com?q=%48%49%50%51%52%53%54");
 
             request.StatusCode.Should().Be(HttpStatusCode.OK);
         }
 
 
-        private static HttpClient CreateClient(int length) {
-            return TestServer.Create(builder => {
-                builder
-                    .MaxQueryStringLength(length)
-                    .Use(async (context, next) => {
-                        context.Response.StatusCode = 200;
-                        context.Response.ReasonPhrase = "OK";
-                    });
-            }).HttpClient;
+        private static HttpClient CreateClient(int length)
+        {
+            return TestServer.Create(builder => builder
+                .MaxQueryStringLength(length)
+                .Use((context, next) =>
+                {
+                    context.Response.StatusCode = 200;
+                    context.Response.ReasonPhrase = "OK";
+                    return Task.FromResult(0);
+                })).HttpClient;
         }
     }
 }
--- src/Owin.Limits/MaxQueryStringLengthMiddleware.cs
+++ src/Owin.Limits/MaxQueryStringLengthMiddleware.cs
@@ -1,18 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.Owin;
+﻿namespace Owin.Limits
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.Owin;
 
-namespace Owin.Limits {
-    internal class MaxQueryStringLengthMiddleware {
-        private readonly Func<IDictionary<string, object>, Task> _next;
+    internal class MaxQueryStringLengthMiddleware
+    {
         private readonly Func<int> _getMaxQueryStringLength;
+        private readonly Func<IDictionary<string, object>, Task> _next;
 
-        public MaxQueryStringLengthMiddleware(Func<IDictionary<string, object>, Task> next, Func<int> getMaxQueryStringLength) {
-            if (next == null) {
+        public MaxQueryStringLengthMiddleware(Func<IDictionary<string, object>, Task> next,
+            Func<int> getMaxQueryStringLength)
+        {
+            if (next == null)
+            {
                 throw new ArgumentNullException("next");
             }
-            if (getMaxQueryStringLength == null) {
+            if (getMaxQueryStringLength == null)
+            {
                 throw new ArgumentNullException("getMaxQueryStringLength");
             }
 
@@ -20,19 +26,25 @@ namespace Owin.Limits {
             _getMaxQueryStringLength = getMaxQueryStringLength;
         }
 
-        public async Task Invoke(IDictionary<string, object> environment) {
-            if (environment == null) {
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            if (environment == null)
+            {
                 throw new ArgumentNullException("environment");
             }
-            
+
             int maxQueryStringLength = _getMaxQueryStringLength();
             var context = new OwinContext(environment);
-            var queryString = context.Request.QueryString;
-            if (queryString.HasValue) {
-                var unescapedQueryString = Uri.UnescapeDataString(queryString.Value);
-                if (unescapedQueryString.Length > maxQueryStringLength) {
+            QueryString queryString = context.Request.QueryString;
+            if (queryString.HasValue)
+            {
+                string unescapedQueryString = Uri.UnescapeDataString(queryString.Value);
+                if (unescapedQueryString.Length > maxQueryStringLength)
+                {
                     context.Response.StatusCode = 414;
-                    context.Response.ReasonPhrase = string.Format("The (unescaped) querystring is too long. Only {0} characters are allowed.", maxQueryStringLength);
+                    context.Response.ReasonPhrase =
+                        string.Format("The (unescaped) querystring is too long. Only {0} characters are allowed.",
+                            maxQueryStringLength);
                     return;
                 }
             }
