506cc727999434017abbd69c68cff9200ec2006f
Damian Hickey <dhickey@gmail.com>
2014-04-03 17:05:41 +0200

    Code cleanup

--- src/Owin.Limits.Tests/MaxContentLengthMiddlewareTests.cs
+++ src/Owin.Limits.Tests/MaxContentLengthMiddlewareTests.cs
@@ -1,5 +1,5 @@
-﻿namespace Owin.Limits {
-    using System;
+﻿namespace Owin.Limits
+{
     using System.IO;
     using System.Net;
     using System.Net.Http;
@@ -8,10 +8,12 @@
     using Microsoft.Owin.Testing;
     using Xunit;
 
-    public class MaxRequestContentLengthMiddlewareTests {
+    public class MaxRequestContentLengthMiddlewareTests
+    {
         [Fact]
-        public async Task When_max_contentLength_is_20_and_a_get_request_is_coming_it_should_be_served() {
-            var requestBuilder = CreateRequest(20);
+        public async Task When_max_contentLength_is_20_and_a_get_request_is_coming_it_should_be_served()
+        {
+            RequestBuilder requestBuilder = CreateRequest(20);
 
             HttpResponseMessage response = await requestBuilder.GetAsync();
 
@@ -20,8 +22,9 @@
 
 
         [Fact]
-        public async Task When_max_contentLength_is_20_and_a_get_request_with_contentLength_header_is_coming_then_the_header_should_be_ignored_and_the_request_served() {
-            var requestBuilder = CreateRequest(20);
+        public async Task When_max_contentLength_is_20_and_a_get_request_with_contentLength_header_is_coming_then_the_header_should_be_ignored_and_the_request_served()
+        {
+            RequestBuilder requestBuilder = CreateRequest(20);
             AddContentLengthHeader(requestBuilder, 10);
 
             HttpResponseMessage response = await requestBuilder.GetAsync();
@@ -30,8 +33,9 @@
         }
 
         [Fact]
-        public async Task When_max_contentLength_is_20_and_a_head_request_is_comig_it_should_be_served() {
-            var requestBuilder = CreateRequest(20);
+        public async Task When_max_contentLength_is_20_and_a_head_request_is_coming_it_should_be_served()
+        {
+            RequestBuilder requestBuilder = CreateRequest(20);
 
             HttpResponseMessage response = await requestBuilder.SendAsync("HEAD");
 
@@ -39,8 +43,9 @@
         }
 
         [Fact]
-        public async Task When_max_contentLength_is_20_and_a_post_with_contentLength_15_is_coming_it_should_be_served() {
-            var requestBuilder = CreateRequest(20);
+        public async Task When_max_contentLength_is_20_and_a_post_with_contentLength_15_is_coming_it_should_be_served()
+        {
+            RequestBuilder requestBuilder = CreateRequest(20);
             requestBuilder.And(req => AddContent(req, 15));
             AddContentLengthHeader(requestBuilder, 15);
 
@@ -50,8 +55,9 @@
         }
 
         [Fact]
-        public async Task When_max_contentLength_is_20_and_a_post_with_contentLength_21_is_coming_it_should_be_rejected() {
-            var requestBuilder = CreateRequest(20);
+        public async Task When_max_contentLength_is_20_and_a_post_with_contentLength_21_is_coming_it_should_be_rejected()
+        {
+            RequestBuilder requestBuilder = CreateRequest(20);
             requestBuilder.And(req => AddContent(req, 21));
             AddContentLengthHeader(requestBuilder, 21);
 
@@ -62,8 +68,9 @@
         }
 
         [Fact]
-        public async Task When_max_contentLength_is_20_and_a_put_with_contentLength_15_is_coming_it_should_be_served() {
-            var requestBuilder = CreateRequest(20);
+        public async Task When_max_contentLength_is_20_and_a_put_with_contentLength_15_is_coming_it_should_be_served()
+        {
+            RequestBuilder requestBuilder = CreateRequest(20);
             requestBuilder.And(req => AddContent(req, 15));
             AddContentLengthHeader(requestBuilder, 15);
 
@@ -73,8 +80,9 @@
         }
 
         [Fact]
-        public async Task When_max_contentLength_is_20_and_a_put_with_contentLength_21_is_coming_it_should_be_rejected() {
-            var requestBuilder = CreateRequest(20);
+        public async Task When_max_contentLength_is_20_and_a_put_with_contentLength_21_is_coming_it_should_be_rejected()
+        {
+            RequestBuilder requestBuilder = CreateRequest(20);
             requestBuilder.And(req => AddContent(req, 21));
             AddContentLengthHeader(requestBuilder, 21);
 
@@ -82,9 +90,11 @@
 
             response.StatusCode.Should().Be(HttpStatusCode.RequestEntityTooLarge);
         }
+
         [Fact]
-        public async Task When_max_contentLength_is_20_and_a_patch_with_contentLength_15_is_coming_it_should_be_served() {
-            var requestBuilder = CreateRequest(20);
+        public async Task When_max_contentLength_is_20_and_a_patch_with_contentLength_15_is_coming_it_should_be_served()
+        {
+            RequestBuilder requestBuilder = CreateRequest(20);
             requestBuilder.And(req => AddContent(req, 15));
             AddContentLengthHeader(requestBuilder, 15);
 
@@ -94,8 +104,9 @@
         }
 
         [Fact]
-        public async Task When_max_contentLength_is_20_and_a_patch_with_contentLength_21_is_coming_it_should_be_rejected() {
-            var requestBuilder = CreateRequest(20);
+        public async Task When_max_contentLength_is_20_and_a_patch_with_contentLength_21_is_coming_it_should_be_rejected()
+        {
+            RequestBuilder requestBuilder = CreateRequest(20);
             requestBuilder.And(req => AddContent(req, 21));
             AddContentLengthHeader(requestBuilder, 21);
 
@@ -105,8 +116,9 @@
         }
 
         [Fact]
-        public async Task When_max_contentLength_is_20_and_the_contentLength_header_in_a_post_is_absent_it_should_be_rejected() {
-            var requestBuilder = CreateRequest(20);
+        public async Task When_max_contentLength_is_20_and_the_contentLength_header_in_a_post_is_absent_it_should_be_rejected()
+        {
+            RequestBuilder requestBuilder = CreateRequest(20);
 
             HttpResponseMessage response = await requestBuilder.PostAsync();
 
@@ -115,8 +127,9 @@
         }
 
         [Fact]
-        public async Task When_max_contentLength_is_20_and_the_contentLength_header_is_not_a_valid_number_it_should_be_rejected() {
-            var requestBuilder = CreateRequest(20);
+        public async Task When_max_contentLength_is_20_and_the_contentLength_header_is_not_a_valid_number_it_should_be_rejected()
+        {
+            RequestBuilder requestBuilder = CreateRequest(20);
             AddContentLengthHeader(requestBuilder, "NotAValidNumber");
 
             HttpResponseMessage response = await requestBuilder.PostAsync();
@@ -126,10 +139,12 @@
         }
 
         [Fact]
-        public async Task When_max_contentLength_is_2_and_the_request_is_chunked_and_longer_it_should_be_rejected() {
-            var request = CreateRequest(2);
+        public async Task When_max_contentLength_is_2_and_the_request_is_chunked_and_longer_it_should_be_rejected()
+        {
+            RequestBuilder request = CreateRequest(2);
 
-            request.And(req => {
+            request.And(req =>
+            {
                 req.Content = new StringContent("4\r\nWiki\r\n5\r\npedia\r\ne\r\nin\r\n\r\nchunks.\r\n0\r\n\r\n");
                 req.Headers.TransferEncodingChunked = true;
             });
@@ -141,27 +156,34 @@
         }
 
 
-        private static RequestBuilder CreateRequest(int maxContentLength) {
-            var server = TestServer.Create(builder => builder
+        private static RequestBuilder CreateRequest(int maxContentLength)
+        {
+            TestServer server = TestServer.Create(builder => builder
                 .MaxRequestContentLength(maxContentLength)
-                .Use(async (context, _) => {
+                .Use(async (context, _) =>
+                {
                     await new StreamReader(context.Request.Body).ReadToEndAsync();
                     await new StreamWriter(context.Response.Body).WriteAsync("Lorem ipsum");
                     context.Response.StatusCode = 200;
                     context.Response.ReasonPhrase = "OK";
                 }));
-            var request = server.CreateRequest("http://example.com");
+            RequestBuilder request = server.CreateRequest("http://example.com");
 
             return request;
         }
 
-        private static void AddContentLengthHeader(RequestBuilder request, int contentLengthValue) {
+        private static void AddContentLengthHeader(RequestBuilder request, int contentLengthValue)
+        {
             AddContentLengthHeader(request, contentLengthValue.ToString());
         }
-        private static void AddContentLengthHeader(RequestBuilder request, string contentLengthValue) {
+
+        private static void AddContentLengthHeader(RequestBuilder request, string contentLengthValue)
+        {
             request.AddHeader("Content-Length", contentLengthValue);
         }
-        private static void AddContent(HttpRequestMessage request, int contentLength) {
+
+        private static void AddContent(HttpRequestMessage request, int contentLength)
+        {
             request.Content = new StringContent("".PadLeft(contentLength, 'a'));
         }
     }
