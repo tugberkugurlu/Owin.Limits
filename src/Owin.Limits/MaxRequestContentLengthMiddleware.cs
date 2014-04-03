69a6d2f3244090fd80e4f35f7310f9818d74a1b7
Damian Hickey <dhickey@gmail.com>
2014-04-03 17:03:46 +0200

    Code cleanup

--- src/Owin.Limits/MaxRequestContentLengthMiddleware.cs
+++ src/Owin.Limits/MaxRequestContentLengthMiddleware.cs
@@ -1,17 +1,25 @@
-﻿namespace Owin.Limits {
+﻿namespace Owin.Limits
+{
     using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using Microsoft.Owin;
+    using Owin.Limits.Annotations;
 
-    internal class MaxRequestContentLengthMiddleware {
-        private readonly Func<IDictionary<string, object>, Task> _next;
+    [UsedImplicitly]
+    internal class MaxRequestContentLengthMiddleware
+    {
         private readonly Func<int> _getMaxContentLength;
-        public MaxRequestContentLengthMiddleware(Func<IDictionary<string, object>, Task> next, Func<int> getMaxContentLength) {
-            if (next == null) {
+        private readonly Func<IDictionary<string, object>, Task> _next;
+
+        public MaxRequestContentLengthMiddleware(Func<IDictionary<string, object>, Task> next, Func<int> getMaxContentLength)
+        {
+            if (next == null)
+            {
                 throw new ArgumentNullException("next");
             }
-            if (getMaxContentLength == null) {
+            if (getMaxContentLength == null)
+            {
                 throw new ArgumentNullException("getMaxContentLength");
             }
 
@@ -20,34 +28,41 @@
         }
 
 
-        public async Task Invoke(IDictionary<string, object> environment) {
-            if (environment == null) {
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            if (environment == null)
+            {
                 throw new ArgumentNullException("environment");
             }
 
             var context = new OwinContext(environment);
-            var request = context.Request;
-            var requestMethod = request.Method.Trim().ToUpper();
+            IOwinRequest request = context.Request;
+            string requestMethod = request.Method.Trim().ToUpper();
 
-            if (requestMethod == "GET" || requestMethod == "HEAD") {
+            if (requestMethod == "GET" || requestMethod == "HEAD")
+            {
                 await _next(environment);
                 return;
             }
-            var maxContentLength = _getMaxContentLength();
-            if (!IsChunkedRequest(request)) {
-                var contentLengthHeaderValue = request.Headers.Get("Content-Length");
-                if (contentLengthHeaderValue == null) {
+            int maxContentLength = _getMaxContentLength();
+            if (!IsChunkedRequest(request))
+            {
+                string contentLengthHeaderValue = request.Headers.Get("Content-Length");
+                if (contentLengthHeaderValue == null)
+                {
                     context.Response.StatusCode = 411;
                     context.Response.ReasonPhrase = "The Content-Length header is missing.";
                     return;
                 }
                 int contentLength;
-                if (!int.TryParse(contentLengthHeaderValue, out contentLength)) {
+                if (!int.TryParse(contentLengthHeaderValue, out contentLength))
+                {
                     context.Response.StatusCode = 400;
                     context.Response.ReasonPhrase = "The Content-Length header value is not a valid number.";
                     return;
                 }
-                if (contentLength > maxContentLength) {
+                if (contentLength > maxContentLength)
+                {
                     context.Response.StatusCode = 413;
                     context.Response.ReasonPhrase = string.Format("The content is too large. It is only a value of {0} allowed.", maxContentLength);
                     return;
@@ -56,19 +71,21 @@
 
             request.Body = new ContentLengthLimitingStream(request.Body, maxContentLength);
 
-            try {
+            try
+            {
                 await _next(environment);
-            } catch (ContentLengthExceededException) {
+            }
+            catch (ContentLengthExceededException)
+            {
                 context.Response.StatusCode = 413;
                 context.Response.ReasonPhrase = string.Format("The content is too large. It is only a value of {0} allowed.", maxContentLength);
             }
         }
-        private static bool IsChunkedRequest(IOwinRequest request) {
-            var header = request.Headers.Get("Transfer-Encoding");
-            if (header == null) {
-                return false;
-            }
-            return header.Equals("chunked", StringComparison.OrdinalIgnoreCase);
+
+        private static bool IsChunkedRequest(IOwinRequest request)
+        {
+            string header = request.Headers.Get("Transfer-Encoding");
+            return header != null && header.Equals("chunked", StringComparison.OrdinalIgnoreCase);
         }
     }
 }
